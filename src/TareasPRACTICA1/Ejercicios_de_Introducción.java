e75bf9eca552f71845d1155473b1f36a860f7509
Danighoul9 <danielddc270606@gmail.com>
2025-10-06 20:09:04 +0200

    yes

--- src/TareasPRACTICA1/Ejercicios_de_Introducción.java
+++ src/TareasPRACTICA1/Ejercicios_de_Introducción.java
@@ -4,34 +4,56 @@ public class Ejercicios_de_Introducción {
     public static void main(String[] args) {
 
         //1. Si A = 6, B = 2, C = 3, indicar el resultado final de las siguientes expresiones aritméticas:
+
         // a. (A * C) % C = 0
         // b. A * B / C = 4
         // c. C % B + C * B = 7
         // d. A % ( A * B * C / (B + C) ) = 6
         // e. B * B + C – B * (A % B ) = 7
 
+//--------------------------------------------------------------------------------------------------------------
 
         // 2. ¿Cuáles son los valores de a y b después de ejecutar las siguientes instrucciones?
+
         // a = 3; ->3
         // b = 4;->4
         // c = 2 * a * b; ->24
         // a = a + 2;-> 6
         // b = c – a;-> 21
-
+//--------------------------------------------------------------------------------------------------------------
 
         //3. Expresar las siguientes expresiones como condiciones lógicas.
+
         // a. a es mayor que 2 ->     a>2
         // b. b es menor o igual que 25 pero mayor que 5 ->     b <= 25 && b > 5
         // c. c es mayor que 6 o igual a d ->     c > 6 || c == d
         // d. e es par menor que 50 ->     e < 50
         // e. f es mayor que a, b y c ->     F > a && F > b && F > c
         // f. g es igual a 3, 4 ó 5 ->    G == 3 || G == 4 || G == 5
+//-------------------------------------------------------------------------------------------------------------
 
         // 4. Decir si son verdaderas o falsas las siguientes expresiones:
-        // a. (37) && (77)
-        // b. !((–81) && (34))
-        // c. “Hola” == “Hola ”
-        // d. ((25) && (36)) || (2–1)
-        // e. ((25) || (36)) && (2–1)
+
+        // a. (3<=7) && (7<=7) -> T && T = T
+        // b. !((–8>1) && (3<=4)) -> !(F && T) -> !(F) -> T
+        // c. “Hola” == “Hola ” -> F
+        // d. ((2<=5) && (3>=6)) || (2>–1) -> T && F || T -> F || T -> T
+        // e. ((2<=5) || (3>=6)) && (2>–1) -> T || F && T -> T && T -> T
+
+//-------------------------------------------------------------------------------------------------------------
+
+
+        // 5. Cumplimenta la siguiente tabla:
+
+        //1.No funciona, porque en el int no tiene sentido las comillas en el valor de la variable
+        //2.Si funciona, mostrara 3,14 ya que es el valor de la variable pi
+        //3.No funciona, porque los decimales en intelliJ es con . no con , ya que es una app inglesa
+        //4.Si funciona, porque esta bien formulado
+        //5.
+        //6.Si funciona
+
+
+
+
     }
 }
