0deba1dfb98a3e7c918f2a11ee32707bba4343d1
Danighoul9 <danielddc270606@gmail.com>
2025-10-16 18:58:16 +0200

    yes
    # Conflicts:
    #	src/EJERCICIOSPRACTICAS/Ejercicio_J_1.java
    
    # Conflicts:
    #	src/TareasTema2/Ejercicio9Bucles.java

---
+++ src/EJERCICIOSPRACTICAS/Ejercicio7.java
@@ -0,0 +1,30 @@
+package EJERCICIOSPRACTICAS;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+
+        //Una persona se encuentra en el kilómetro 70 de una carretera,
+        // otra se encuentra en el km 150, los coches tienen sentido opuesto
+        // y tienen la misma velocidad. Realizar un programa para determinar
+        // en qué kilómetro de esa carretera se encontrarán
+
+
+
+        int kilometroCoche1 = 70;
+        int kilometroCoche2 = 150;
+
+        for(kilometroCoche1=70; kilometroCoche1 <= kilometroCoche2; kilometroCoche1++){
+
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
